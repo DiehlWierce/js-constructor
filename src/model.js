b77f24ca4fa92d1577959e3e7b25ab287358cf1a
Diehl Wierce <johnbrotherwood@gmail.com>
2020-12-09 14:43:35 +0300

    add classes + refactor models

--- src/model.js
+++ src/model.js
@@ -1,24 +1,50 @@
 import image from "./assets/image.png";
+import { Block } from "./classes/blocks";
 
 export const model = [
-  {
-    type: "title",
-    value: "Конструктор сайтов на чистом JS",
-    options: {
-      tag: "h2",
-      styles: `background: linear-gradient(to right, #ff0099, #493240); color: #fff; text-align: center; padding: 1.5rem; user-select: none;`,
+  new Block("title", "Конструктор сайтов на чистом JS", {
+    tag: "h2",
+    styles: {
+      background: "linear-gradient(to right, #ff0099, #493240)",
+      color: "#fff",
+      "text-align": "center",
+      padding: "1.5rem",
+      "user-select": "none",
     },
-  },
-  {
-    type: "text",
-    value: "this text from another model type",
-  },
-  {
-    type: "columns",
-    value: ["111111111", "222222222", "333333333"],
-  },
-  {
-    type: "image",
-    value: image,
-  },
+  }),
+  new Block("image", image, {
+    styles: {
+      padding: "2rem 0",
+      display: "grid",
+      "justify-content": "center",
+    },
+    alt: "Это картинка",
+    imageStyles: {
+      width: "500px",
+      height: "auto",
+    },
+  }),
+  new Block(
+    "columns",
+    [
+      "Приложение на чистом JS, без использования библиотек",
+      "Узнаешь, как работают принципы SOLID и ОПП в JS за один курс",
+      "JS - это просто, интересно. Научись создавать любые UI своими руками",
+    ],
+    {
+      styles: {
+        background: "linear-gradient(to bottom, #8e2de2, #4a00e0)",
+        padding: "2rem",
+        color: "#fff",
+        "font-weight": "bold",
+      },
+    }
+  ),
+  new Block("text", "this text from another model type", {
+    styles: {
+      background: "linear-gradient(to left, #f2994a, #f2c94c)",
+      padding: "1rem",
+      "font-weight": "bold",
+    },
+  }),
 ];
--- src/templates.js
+++ src/templates.js
@@ -1,23 +1,29 @@
-import { row, col } from "./utils";
+import { row, col, css } from "./utils";
 
 function title(block) {
   const { tag = "h1", styles } = block.options;
-  return row(col(`<${tag}>${block.value}</${tag}>`), styles);
+  return row(col(`<${tag}>${block.value}</${tag}>`), css(styles));
 }
 
 function text(block) {
-  return row(col(`<p>${block.value}</p>`));
+  const { styles } = block.options;
+  return row(col(`<p>${block.value}</p>`), css(styles));
 }
 
 //в мап передается функция в которую кидается колбек
 //join для удаления запятых
 function columns(block) {
+  const { styles } = block.options;
   const html = block.value.map(col).join("");
-  return row(html);
+  return row(html, css(styles));
 }
 
 function image(block) {
-  return row(col(`<img src=${block.value} />`));
+  const { styles, imageStyles, alt } = block.options;
+  return row(
+    col(`<img src=${block.value} alt='${alt}' style='${css(imageStyles)}' />`),
+    css(styles)
+  );
 }
 
 //т.к. ключ и название функции - значение - совпадают, то пишем одно слово
